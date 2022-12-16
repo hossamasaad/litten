67a44dee859236d1253f25e7307c5fc77a6cdd65
hossamasaad <hossamasaad10@gmail.com>
2022-12-17 00:29:02 +0200

    Draw more layers, make layer width more flixable using start_x and end_x

--- litten/draw/draw.py
+++ litten/draw/draw.py
@@ -7,29 +7,42 @@ from litten.layers import *
 import PIL.Image as Image
 from PIL import ImageDraw, ImageFont
 
-no_of_layers = 8
+no_of_layers = 15
 h = 320
 w = (4 * no_of_layers) * 20
 
 image = Image.new("RGB", (w, h), color = '#ffffff')
 
-layer1 = InputLayer("Input", "(5,6)", 1)
-layer2 = Layer("XD Layer",  2)
-layer3 = DenseLayer("Dense", 3, 60)
-layer4 = CNNLayer("CNN", 4)
-layer5 = PoolingLayer("Pooling", 5)
-layer6 = NormaliztionLayer("BatchNormalization", 6)
-layer7 = ActivationLayer("BatchNormalization", 7)
+layer1 = InputLayer("Input", 0)
+layer1.draw(image)
 
+layer2 = Layer("Input", layer1.end)
+layer2.draw(image)
 
+layer3 = DenseLayer("Dense", layer2.end, 100)
+layer3.draw(image)
 
-layer1.draw(image, show_properties=True)
-layer2.draw(image, show_properties=True)
-layer3.draw(image, show_properties=False)
+layer4 = CNNLayer("Dense", layer3.end, 1024, (500, 500, 3))
 layer4.draw(image)
+
+layer5 = CNNLayer("Dense", layer4.end, 64, (250, 250, 3))
 layer5.draw(image)
+
+
+layer6 = PoolingLayer("Dense", layer5.end, 64, (75, 75, 3))
 layer6.draw(image)
+
+layer7 = ActivationLayer("Dense", layer6.end,)
 layer7.draw(image)
+
+layer8 = EmbeddingLayer("Dense", layer7.end,)
+layer8.draw(image)
+
+layer9 = RecurrentLayer("Dense", layer8.end, bi=True)
+layer9.draw(image)
+
+layer10 = RecurrentLayer("Dense", layer9.end, bi=False)
+layer10.draw(image)
 
 image.show()
 
--- litten/layers/layers.py
+++ litten/layers/layers.py
@@ -1,10 +1,11 @@
+import math
 from PIL import ImageDraw, ImageFont 
 
 class Layer:
     """
     Layer class contain the base structure for any layer
     """
-    def __init__(self, name, number) -> None:
+    def __init__(self, name, start_x) -> None:
         """
         Construct Layer class
 
@@ -12,28 +13,32 @@ class Layer:
             name: layer name
             number: layer number
         """
-        self.name = name
-        self.number = number
-    
+        self.name    = name
+        self._start_x = start_x
+        self._end_x   = 0
+
     def draw(self, image, show_properties=False):
         """
         draw default layer 
         """
         draw = ImageDraw.Draw(image)
-        points = [(4 * (self.number - 1) * 20 + 20, 80),
-                  (4 * (self.number - 1) * 20 + 60, 200)]
+        points = [(self._start_x + 20, 80),
+                  (self._start_x + 60, 200)]
         
-        draw.rectangle(xy  = points, fill='#98c1d9', width=1,outline='#000000')
+        draw.rounded_rectangle(xy  = points, radius=5, fill='#94a4b2')
         
-        draw.ellipse((points[0][0] + 10, points[0][1] + 15 , points[0][0] + 30, points[0][1] + 35), fill = '#ffffff', outline='#ffffff')
-        draw.ellipse((points[0][0] + 10, points[0][1] + 50 , points[0][0] + 30, points[0][1] + 70), fill = '#ffffff', outline='#ffffff')
-        draw.ellipse((points[0][0] + 10, points[0][1] + 85, points[0][0] + 30, points[0][1] + 105), fill = '#ffffff', outline='#ffffff')
+        draw.ellipse((points[0][0] + 10, points[0][1] + 15, points[0][0] + 30, points[0][1] + 35 ), fill = '#ffffff', outline='#000')
+        draw.ellipse((points[0][0] + 10, points[0][1] + 50, points[0][0] + 30, points[0][1] + 70 ), fill = '#ffffff', outline='#000')
+        draw.ellipse((points[0][0] + 10, points[0][1] + 85, points[0][0] + 30, points[0][1] + 105), fill = '#ffffff', outline='#000')
 
-        if show_properties:
-            fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 16)            
-            draw.multiline_text((points[0][0] - 10, points[1][1] + 10), "{}".format(self.name ), fill='#000000', font=fnt)
+        # update end_x
+        self._end_x = self._start_x + 80
 
         return image
+
+    @property
+    def end(self):
+        return self._end_x
 
     def show_prop(self):
         pass
@@ -43,8 +48,8 @@ class InputLayer(Layer):
     """
     Input Layer
     """
-    def __init__(self, name, shape, number) -> None:
-        super().__init__(name, number)
+    def __init__(self, name, shape, start_x = 0) -> None:
+        super().__init__(name, start_x)
         self.shape = shape
     
     def draw(self, image, show_properties = False):
@@ -60,14 +65,11 @@ class InputLayer(Layer):
         draw = ImageDraw.Draw(image)
 
         for i in range(10):
-            points = [(20, 40 + i * 20), (40, 60 + i * 20)] 
-            draw.rectangle(points, fill = "#98c1d9", width = 2, outline= '#000000')
+            points = [(self._start_x + 20, 40 + i * 20), (self._start_x + 40, 60 + i * 20)] 
+            draw.rectangle(points, fill = "#94a4b2", width = 2, outline= '#000000')
 
-
-        if show_properties:
-            fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 16)            
-            draw.multiline_text((points[0][0] - 10, points[0][1] + 25), "{}".format(self.name ), fill='#000000', font=fnt)
-            draw.multiline_text((points[0][0] - 15, points[0][1] + 40), "{}".format(self.shape), fill='#000000', font=fnt)
+        # update end_x
+        self._end_x = self._start_x + 60
 
         return image
 
@@ -76,8 +78,8 @@ class InputLayer(Layer):
 
 
 class DenseLayer(Layer):
-    def __init__(self, name, number, units) -> None:
-        super().__init__(name, number)
+    def __init__(self, name, start_x, units) -> None:
+        super().__init__(name, start_x)
         self.units = units
 
     def draw(self, image, show_properties=False):
@@ -91,9 +93,12 @@ class DenseLayer(Layer):
         """
         draw = ImageDraw.Draw(image)
 
-        draw.ellipse((4 * (self.number - 1) * 20 + 20, 60 , 4 * (self.number - 1) * 20 + 60, 100), fill = '#98c1d9', outline='#000000')
-        draw.ellipse((4 * (self.number - 1) * 20 + 20, 120, 4 * (self.number - 1) * 20 + 60, 160), fill = '#98c1d9', outline='#000000')
-        draw.ellipse((4 * (self.number - 1) * 20 + 20, 180, 4 * (self.number - 1) * 20 + 60, 220), fill = '#98c1d9', outline='#000000')
+        draw.ellipse((self._start_x + 20, 60 , self._start_x + 60, 100), fill = '#94a4b2', outline='#000000')
+        draw.ellipse((self._start_x + 20, 120, self._start_x + 60, 160), fill = '#94a4b2', outline='#000000')
+        draw.ellipse((self._start_x + 20, 180, self._start_x + 60, 220), fill = '#94a4b2', outline='#000000')
+        
+        # update end_x
+        self._end_x = self._start_x + 80
 
         return image
 
@@ -102,55 +107,100 @@ class DenseLayer(Layer):
 
 
 class CNNLayer(Layer):
-    def __init__(self, name, number) -> None:
-        super().__init__(name, number)
-    
+    def __init__(self, name, start_x, kernels, shape) -> None:
+        super().__init__(name, start_x)
+        self.units = kernels
+        self.shape = shape
+        self._c    = int(min(math.log(kernels, 2), 10))
+        self._s    = min(int(shape[0]/100), 10) 
+
     def draw(self, image, show_properties=False):
         draw = ImageDraw.Draw(image)
+        points = [self._start_x + 30                     , 110 - 5 * self._c // 2 - 10 * self._s // 2,
+                  self._start_x + 100 + 10 * self._s, 170 - 5 * self._c // 2 + 10 * self._s // 2]
 
-        draw.rectangle((4 * (self.number - 1) * 20 + 10, 170, 4 * (self.number - 1) * 20 + 70, 110), fill = '#94a4b2', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 15, 175, 4 * (self.number - 1) * 20 + 75, 115), fill = '#a2dbf9', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 20, 180, 4 * (self.number - 1) * 20 + 80, 120), fill = '#94a4b2', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 25, 185, 4 * (self.number - 1) * 20 + 85, 125), fill = '#a2dbf9', outline='#000000')
+        for i in range(self._c):
+            draw.rectangle(points, fill = '#94a4b2', outline='#000000')
+            points[0], points[1], points[2], points[3] = points[0] + 5, points[1] + 5, points[2] + 5, points[3] + 5
+            
+            
+        # update end_x
+        self._end_x = points[2] + 20
 
         return image
 
 
 class PoolingLayer(Layer):
-    def __init__(self, name, number) -> None:
-        super().__init__(name, number)
-    
+    def __init__(self, name, start_x, kernels, shape) -> None:
+        super().__init__(name, start_x)
+        self.units = kernels
+        self.shape = shape
+        self._c    = int(min(math.log(kernels, 2), 10))
+        self._s    = int(shape[0]/100) 
+
     def draw(self, image, show_properties=False):
         draw = ImageDraw.Draw(image)
+        points = [self._start_x + 30                     , 110 - 5 * self._c // 2 - 10 * self._s,
+                  self._start_x + 100 + 10 * self._s // 2, 170 - 5 * self._c // 2 + 10 * self._s]
 
-        draw.rectangle((4 * (self.number - 1) * 20 + 20, 160, 4 * (self.number - 1) * 20 + 60, 120), fill = '#94a4b2', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 25, 165, 4 * (self.number - 1) * 20 + 65, 125), fill = '#a2dbf9', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 30, 170, 4 * (self.number - 1) * 20 + 70, 130), fill = '#94a4b2', outline='#000000')
-        draw.rectangle((4 * (self.number - 1) * 20 + 35, 175, 4 * (self.number - 1) * 20 + 75, 135), fill = '#a2dbf9', outline='#000000')
+        for i in range(self._c):
+            draw.rectangle(points, fill = '#94a4b9', outline='#000000')
+            points[0], points[1], points[2], points[3] = points[0] + 5, points[1] + 5, points[2] + 5, points[3] + 5
+            
+            
+        # update end_x
+        self._end_x = points[2] + 20
 
         return image
 
-    def show_prop(self):
-        pass
-
 
 class EmbeddingLayer(Layer):
-    def __init__(self, name, number) -> None:
-        super().__init__(name, number)
+    def __init__(self, name, start_x) -> None:
+        super().__init__(name, start_x)
     
     def draw(self, image, show_properties=False):
-        return super().draw(image, show_properties)
+        draw = ImageDraw.Draw(image)
+        draw.rounded_rectangle((self._start_x + 20, 60, self._start_x + 60, 200), radius=5, fill = '#94a4b9', outline='#000000')
+
+        # update end_x
+        self._end_x = self._start_x + 80
+        return image
 
     def show_prop(self):
         pass
 
 
 class RecurrentLayer(Layer):
-    def __init__(self, name, number) -> None:
-        super().__init__(name, number)
-    
+    def __init__(self, name, start_x, bi=False) -> None:
+        super().__init__(name, start_x)
+        self.bi = bi
+        
     def draw(self, image, show_properties=False):
-        return super().draw(image, show_properties)
+        draw = ImageDraw.Draw(image)
+        
+        points1 = [ self._start_x + 40, 100, self._start_x + 120, 160]
+        points2 = [(self._start_x + 40, 100), (self._start_x + 30, 90), (self._start_x + 30, 150), (self._start_x + 40, 160)]
+        points3 = [(self._start_x + 40, 100), (self._start_x + 30, 90), (self._start_x + 110, 90), (self._start_x + 120, 100)]
+
+        draw.rectangle(points1, fill = '#94a4b9', outline='#000000')
+        draw.polygon  (points2, fill = '#94a4b9', outline='#000000')
+        draw.polygon  (points3, fill = '#94a4b9', outline='#000000')
+
+        # draw arrows
+        points5 = [(self._start_x + 60 , 120), (self._start_x + 100, 120), (self._start_x + 95, 115), (self._start_x + 100, 120), (self._start_x + 95, 125)]
+        points6 = [(self._start_x + 100, 140), (self._start_x + 60 , 140), (self._start_x + 65, 135), (self._start_x + 60 , 140), (self._start_x + 65, 145)]
+
+        points7 = [(self._start_x + 60 , 130), (self._start_x + 100, 130), (self._start_x + 95, 125), (self._start_x + 100, 130), (self._start_x + 95, 135)]
+
+        if self.bi:
+            draw.line(points5, fill="#000000")
+            draw.line(points6, fill="#000000")
+        else:
+            draw.line(points7, fill="#000000")
+
+        # update end_x
+        self._end_x = self._start_x + 140
+        return image
 
     def show_prop(self):
         pass
@@ -196,18 +246,18 @@ class RegularizationLayer(Layer):
         pass
 
 
-
-
 class ActivationLayer(Layer):
-    def __init__(self, name, number) -> None:
-        super().__init__(name, number)
+    def __init__(self, name, start_x) -> None:
+        super().__init__(name, start_x)
     
     def draw(self, image, show_properties=False):
         draw = ImageDraw.Draw(image)
 
-        draw.rectangle((4 * (self.number - 1) * 20 + 20, 160, 4 * (self.number - 1) * 20 + 60, 120), fill = '#98c1d9', outline='#000000')
-        draw.ellipse  ((4 * (self.number - 1) * 20 + 25, 125, 4 * (self.number - 1) * 20 + 55, 155), fill = '#ffffff', outline='#000000')
+        draw.rectangle((self._start_x + 20, 160, self._start_x + 60, 120), fill = '#94a4b9')
+        draw.ellipse  ((self._start_x + 25, 125, self._start_x + 55, 155), fill = '#ffffff', outline='#000000')
 
+        # update end_x
+        self._end_x = self._start_x + 70
         return image
 
     def show_prop():
